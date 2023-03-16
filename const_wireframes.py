f4b41e5e2582f074df32866a92aeb4a1ec7fc9e5
JPLdeCamargo <jplopescamargo@gmail.com>
2023-03-16 08:17:54 -0300

    Refactor

--- const_wireframes.py
+++
@@ -1,6 +0,0 @@
-from game_objects.coords2D import Coords2d
-from game_objects.wireframe import WireFrame
-
-y = 2000
-x = 5000
-basic_square = WireFrame([Coords2d(x, x), Coords2d(y, x), Coords2d(y, y), Coords2d(x, y)])
