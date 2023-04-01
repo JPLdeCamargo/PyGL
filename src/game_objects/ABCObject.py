238b000d6ec4ba27df4f3a4d865daee3fbbb49a7
JPLdeCamargo <jplopescamargo@gmail.com>
2023-04-01 19:11:40 -0300

    começo da entrega 3

--- src/game_objects/ABCObject.py
+++ src/game_objects/ABCObject.py
@@ -3,15 +3,20 @@ from .coords2D import Coords2d
 from .cg_math import CgMath
 
 class ABCObject(ABC):
-    def __init__(self, name: str, color : tuple[int]) -> None:
+    def __init__(self, name: str, color : tuple[int], coords: list[Coords2d]) -> None:
         super().__init__()
         self.__name = name
         self.__color = color
+        self.__coords = coords
+        self.__normalized_coords = []
 
     @property
-    @abstractmethod
     def coords(self) -> list[Coords2d]:
-        pass
+        return self.__coords
+
+    @property
+    def normalized_coords(self) -> list[Coords2d]:
+        return self.__normalized_coords
 
     @property
     @abstractmethod
@@ -25,6 +30,16 @@ class ABCObject(ABC):
     @property
     def color(self) -> tuple[int]:
         return self.__color
+
+    def update_normalized(self, normalized_m:list[list[float]]):
+        self.__normalized_coords = []
+        for point in self.coords:
+            homogenous = [[point.x, point.y, 1]]
+            transformed = CgMath.matrix_multiply(homogenous, normalized_m)
+            x = transformed[0][0]
+            y = transformed[0][1]
+            self.__normalized_coords.append(Coords2d(x, y))
+
     
     def rotate(self, degree_angle : float, anchor : Coords2d):
         matrix = self.__get_center_rotation_matrix(anchor, degree_angle)
@@ -43,11 +58,23 @@ class ABCObject(ABC):
         self.__apply_matrix_transform(matrix)
 
     def __apply_matrix_transform(self, matrix :list[list[int]]):
-        for point in self.coords:
+        for i in range(len(self.__coords)):
+            point = self.__coords[i]
+
             homogenous = [[point.x, point.y, 1]]
             transformed = CgMath.matrix_multiply(homogenous, matrix)
-            point.x = transformed[0][0]
-            point.y = transformed[0][1]
+            self.__coords[i].x = transformed[0][0]
+            self.__coords[i].y = transformed[0][1]
+
+            if len(self.__normalized_coords) != 0:
+                norm_point = self.__normalized_coords[i]
+
+                homogenous = [[norm_point.x, norm_point.y, 1]]
+                transformed = CgMath.matrix_multiply(homogenous, matrix)
+                norm_point.x = transformed[0][0]
+                self.__normalized_coords[i].x = transformed[0][0]
+                self.__normalized_coords[i].y = transformed[0][1]
+
 
 
     def __get_center_rotation_matrix(self, center: Coords2d, degree_angle : float):
--- src/window.py
+++ src/window.py
@@ -1,22 +1,31 @@
 from .game_objects.ABCObject import ABCObject
-
+from .game_objects.coords2D import Coords2d
+from .game_objects.cg_math import CgMath
 from .const_wireframes import to_load
+
+
+import math
+
+
 class Window:
-    def __init__(self, max_x:float, max_y:float) -> None:
+    def __init__(self, x_size : float, y_size : float) -> None:
         self.__display_file = to_load
-        self.__max_x = max_x
-        self.__max_y = max_y
+
+        self.__size = Coords2d(x_size, y_size)
+        self.__center = self.__scale / 2
+        self.__up_vector = Coords2d(0, 1)
+
         self.__min_x = 0
         self.__min_y = 0
 
-        self.__ratio = max_x/max_y
+        self.__ratio = self.__size.x/self.__size.y
 
         # For checking names in constant time
         self.__names_map = {}
         for obj in self.__display_file:
             self.__names_map[obj.name] = obj
 
-
+        
     @property
     def display_file(self) -> list[ABCObject]:
         return self.__display_file
@@ -68,3 +77,27 @@ class Window:
         if name in self.__names_map:
             return self.__names_map[name]
         return False
+
+    def update_normalized(self):
+        abs_up = Coords2d(1, 0)
+
+        # Rotating
+        angle = math.acos(abs_up.x * self.__up_vector.x + abs_up.y * self.__up_vector.y)
+        print(angle)
+        degrees = (angle/math.pi) * 180
+        translate_center_m = CgMath.get_translation_matrix(self.__center.x, self.__center.y)
+        rotation_m = CgMath.get_rotation_matrix(degrees)
+        center_rotation_m = CgMath.matrix_multiply(translate_center_m, rotation_m)
+
+        # Normalizing -1, 1
+        scale_x = 1/self.__size.x
+        scale_y = 1/self.__size.y
+        scale_m = CgMath.get_scale_matrix(scale_x, scale_y)
+        translate_m = CgMath.get_translation_matrix(-1, -1)
+        normalization_m = CgMath.matrix_multiply(scale_m, translate_m)
+
+        full_normalize_transform = CgMath.matrix_multiply(center_rotation_m, normalization_m)
+
+        for obj in self.__display_file:
+            obj.update_normalized(full_normalize_transform)
+
